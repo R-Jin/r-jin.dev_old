14eae582504be4b29a1a47e2f8902b29acb63d79
Ryan Jin <ryanjin007@gmail.com>
2022-10-26 00:35:13 +0200

    Stop displaying the last horizontal rule
    Hide the last horizontal rule in the LastPost component

--- components/latestPosts/latestPosts.tsx
+++ components/latestPosts/latestPosts.tsx
@@ -15,29 +15,31 @@ export default function  LatestPosts({allPostsData}: {
 
             <ul>
                 {allPostsData.slice(0, 3).map((post) => (
-                    <Link href={`/posts/${post.id}`} key={post.id}>
-                        <a>
-                            <li>
-                                <h1 className="font-sans text-2xl font-extrabold text-darkAccent">
-                                    {post.title}
-                                </h1>
-                                <small className="font-serif text-sm">
-                                    {post.date}
-                                </small>
-                                <p className="font-serif text-sm pt-1">
-                                    {post.metaDesc}
-                                </p>
-                                <ul className="grid grid-cols-4 w-1/3 pt-3">
-                                    {post.tags.map((tag) => (
-                                        <li key={tag} className="font-serif text-xs underline">
-                                            {tag}
-                                        </li>
-                                    ))}
-                                </ul>
-                                <hr className="text-darkforeground-100 my-8"/>
-                            </li>
-                        </a>
-                    </Link>
+                    <>
+                        <Link href={`/posts/${post.id}`} key={post.id}>
+                            <a>
+                                <li>
+                                    <h1 className="font-sans text-2xl font-extrabold text-darkAccent">
+                                        {post.title}
+                                    </h1>
+                                    <small className="font-serif text-sm">
+                                        {post.date}
+                                    </small>
+                                    <p className="font-serif text-sm pt-1">
+                                        {post.metaDesc}
+                                    </p>
+                                    <ul className="grid grid-cols-4 w-1/3 pt-3">
+                                        {post.tags.map((tag) => (
+                                            <li key={tag} className="font-serif text-xs underline">
+                                                {tag}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+                            </a>
+                        </Link>
+                        <hr className="text-darkforeground-100 my-8 last:hidden"/>
+                    </>
                     // <li key={post.id}>
                     //     <h1 className="font-sans text-2xl font-extrabold text-darkAccent">
                     //         <Link href={`/posts/${post.id}`}>
