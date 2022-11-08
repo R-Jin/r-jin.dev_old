e1e7615fd1149e0ad927ce2b6f06b7e41dfd794a
Ryan Jin <ryanjin007@gmail.com>
2022-11-09 00:03:13 +0100

    Create component that renders all the projects

---
+++ components/projects/allProjects.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+export default function AllProjects({allProjectsData}: {
+    allProjectsData: {
+        metaDesc: string
+        date: string
+        title: string
+        id: string
+        tags: string[]
+    }[]
+}) {
+    return (
+            <ul className="mt-20">
+                {allProjectsData.map((project) => (
+                    <div key={project.id}>
+                        <Link href={`/projects/${project.id}`}>
+                            <a>
+                                <li>
+                                    <h1 className="font-sans text-2xl font-extrabold text-darkAccent">
+                                        {project.title}
+                                    </h1>
+                                    <small className="font-serif text-sm">
+                                        {project.date}
+                                    </small>
+                                    <p className="font-serif text-sm pt-1">
+                                        {project.metaDesc}
+                                    </p>
+                                    <ul className="grid grid-cols-4 w-1/3 pt-3">
+                                        {project.tags.map((tag) => (
+                                            <li key={tag} className="font-serif text-xs underline">
+                                                {tag}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
+                            </a>
+                        </Link>
+                        <hr className="text-darkforeground-100 my-8"/>
+                    </div>
+                ))}
+            </ul>
+    )
+}
