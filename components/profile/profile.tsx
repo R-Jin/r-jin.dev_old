0a8d1892f4d237726fa20372c00fe6329e88f60a
Ryan Jin <ryanjin007@gmail.com>
2022-11-09 00:01:04 +0100

    Move some text to the about section

--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -4,19 +4,16 @@ import Socials from './socials'
 
 const Profile = () => {
     return (
-        <div className='flex flex-col py-10 items-center space-y-10 md:flex-row md:space-x-[50px] md:space-y-0 md:py-28'>
-            <div className='rounded-full w-[250px] border-[14px] dark:border-darkLightBackground shadow-lg md:w-[500px]'>
+        <div className='flex flex-col pt-10 items-center space-y-10 md:flex-row md:space-x-[50px] md:space-y-0 md:pt-28'>
+            <div className='rounded-full w-[250px] border-[14px] dark:border-darkLightBackground shadow-lg md:w-[500px] flex-auto'>
                 <Image src="/profile.jpg" layout='responsive' width={900} height={900} alt="Profile picture" className='rounded-full' priority/>
             </div>
 
-            <div className="flex flex-col justify-self-center">
+            <div className="flex flex-col justify-self-center flex-auto md:w-[1200px]">
                 <h3 className='font-serif text-sm dark:text-darkAccent pb-1 md:text-base'>Hi, my name is</h3>
                 <h1 className='font-sans font-extrabold text-4xl pb-4 md:text-6xl'>Ryan Jin</h1>
                 <p className='font-serif text-base md:text-xl'>
                     I&apos;m a <span className='dark:text-darkAccent'>Computer Science Student</span> based in Gothenburg, Sweden. 
-                    Currently, I&apos;m focused on <span className='dark:text-darkAccent'>simulation models</span>, 
-                    <span className='dark:text-darkAccent'> web development</span> and topics related 
-                    to <span className='dark:text-darkAccent'>Linux</span>.
                 </p>
 
                 <Socials />
