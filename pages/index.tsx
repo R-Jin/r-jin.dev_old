9cf7384fa6fb33022034ec1796930ca7d521e994
Ryan Jin <ryanjin007@gmail.com>
2023-01-29 11:29:13 +0100

    Change about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,10 +25,9 @@ const Home: NextPage = ({allPostsData}: any) => {
         <div className='my-20'>
           <h1 className='text-3xl font-sans font-bold pb-2'>About Me</h1>
           <p className='font-serif text-lg'>
-              Currently, I&apos;m focused on <span className='text-blueAccent dark:text-darkAccent'> web development</span> 
-              ,  and topics related to <span className='text-blueAccent dark:text-darkAccent'> Linux</span> and 
-              <span className='text-blueAccent dark:text-darkAccent'> computer science</span>.
-              In my free time I enjoy playing badminton, reading, playing video games.           
+              Currently, I&apos;m focused on <span className='text-blueAccent dark:text-darkAccent'> web development </span> 
+               and topics related to <span className='text-blueAccent dark:text-darkAccent'> Linux</span>. 
+              In my free time I enjoy playing badminton, reading and exploring the field of computer science.           
           </p>
         </div>
         <LatestPosts allPostsData={allPostsData}/>
